a96060b094df1aef7294b4830d901952c224f974
Chris Gibbons <chris.gibbons@coop.co.uk>
2021-01-08 11:52:27 +0000

    Adds in icon for sub nav items + animation

--- src/_js/main.js
+++ src/_js/main.js
@@ -2,4 +2,5 @@ var storeForms = new storeForms();
 
 $('.toggle-link').click(function () {
   $(this).next('.coop-side-nav--nested').toggle();
+  $(this).find('.coopssn-c-nav__link__icon__svg').toggleClass('is-open');
 });
