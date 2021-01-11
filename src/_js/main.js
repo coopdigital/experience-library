893a9c480f7ae4cc52c6d8598fab507520cc96f0
Chris Gibbons <chris.gibbons@coop.co.uk>
2021-01-11 11:37:00 +0000

    Fakes active naviagation

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,6 +1,15 @@
 var storeForms = new storeForms();
 
 $('.toggle-link').click(function () {
-  $(this).next('.coop-side-nav--nested').toggle();
-  $(this).find('.coopssn-c-nav__link__icon__svg').toggleClass('is-open');
+  if (!$(this).hasClass('is-open')) {
+    $(this).addClass('is-open');
+    $(this).next('.coop-side-nav--nested').toggle();
+  } else {
+    $(this).removeClass('is-open');
+  }
 });
+
+if ($('.toggle-link').hasClass('is-open')) {
+  console.log('has class!');
+  $('.toggle-link.is-open').next('.coop-side-nav--nested').show();
+}
