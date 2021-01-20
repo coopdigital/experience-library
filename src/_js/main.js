976ec17e516148197498594b96551f5dba6799cf
Chris Gibbons <chris.gibbons@coop.co.uk>
2021-01-20 14:38:49 +0000

    Adds in some search

--- src/_js/main.js
+++ src/_js/main.js
@@ -11,6 +11,28 @@ $('.toggle-link').click(function () {
 });
 
 if ($('.toggle-link').hasClass('is-open')) {
-  console.log('?');
   $('.toggle-link.is-open').next('.coop-side-nav--nested').show();
 }
+
+$('#search-small').click(function (e) {
+  e.preventDefault();
+  $('.coop-c-search__modal-bg').show();
+  $('.coop-c-search__form').show();
+  $('body').addClass('modal-open');
+});
+
+$(document).keyup(function (e) {
+  if (e.which == 27 && $('.coop-c-search__modal-bg').is(':visible')) {
+    $('.coop-c-search__modal-bg').hide();
+    $('.coop-c-search__form').hide();
+    $('body').removeClass('modal-open');
+  }
+});
+
+$('.coop-c-search__modal-bg').click(function (e) {
+  if ($('.coop-c-search__modal-bg').is(':visible')) {
+    $('.coop-c-search__modal-bg').hide();
+    $('.coop-c-search__form').hide();
+    $('body').removeClass('modal-open');
+  }
+});
