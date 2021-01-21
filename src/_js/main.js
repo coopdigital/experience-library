36f4d7831c1416533a16297974c6433273dbf274
Chris Gibbons <chris.gibbons@coop.co.uk>
2021-01-21 22:06:29 +0000

    Moves breadcrumb into sidenav and tweaks search

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,38 +1,46 @@
 var storeForms = new storeForms();
 
-$('.toggle-link').click(function () {
-  if (!$(this).hasClass('is-open')) {
-    $(this).addClass('is-open');
-    $(this).next('.coop-side-nav--nested').toggle();
+$(function () {
+  $('.toggle-link').click(function () {
+    if (!$(this).hasClass('is-open')) {
+      $(this).addClass('is-open');
+      $(this).next('.coop-side-nav--nested').toggle();
+    } else {
+      $(this).removeClass('is-open');
+      $(this).next('.coop-side-nav--nested').toggle();
+    }
+  });
+
+  if ($('.toggle-link').hasClass('is-open')) {
+    $('.toggle-link.is-open').next('.coop-side-nav--nested').show();
+  }
+
+  // $('#search-small').click(function (e) {
+  //   e.preventDefault();
+  //   $('.coop-c-search__modal-bg').show();
+  //   $('.coop-c-search__form').show();
+  //   $('body').addClass('modal-open');
+  // });
+
+  $(document).keyup(function (e) {
+    if (e.which == 27 && $('#results-container').is(':visible')) {
+      $('.coop-c-search__modal-bg').hide();
+      $('.coop-c-search__form').hide();
+      $('body').removeClass('modal-open');
+    }
+  });
+
+  $('.coop-c-search__results-bg').click(function (e) {
+    if ($('.coop-c-search__results-bg').is(':visible')) {
+      $('.coop-c-search__results-bg').hide();
+      $('#results-container').hide();
+      $('body').removeClass('modal-open');
+    }
+  });
+
+  if ($('.coop-search-results:empty')) {
+    console.log(':(');
   } else {
-    $(this).removeClass('is-open');
-    $(this).next('.coop-side-nav--nested').toggle();
-  }
-});
-
-if ($('.toggle-link').hasClass('is-open')) {
-  $('.toggle-link.is-open').next('.coop-side-nav--nested').show();
-}
-
-$('#search-small').click(function (e) {
-  e.preventDefault();
-  $('.coop-c-search__modal-bg').show();
-  $('.coop-c-search__form').show();
-  $('body').addClass('modal-open');
-});
-
-$(document).keyup(function (e) {
-  if (e.which == 27 && $('.coop-c-search__modal-bg').is(':visible')) {
-    $('.coop-c-search__modal-bg').hide();
-    $('.coop-c-search__form').hide();
-    $('body').removeClass('modal-open');
-  }
-});
-
-$('.coop-c-search__modal-bg').click(function (e) {
-  if ($('.coop-c-search__modal-bg').is(':visible')) {
-    $('.coop-c-search__modal-bg').hide();
-    $('.coop-c-search__form').hide();
-    $('body').removeClass('modal-open');
+    console.log(':)');
   }
 });
