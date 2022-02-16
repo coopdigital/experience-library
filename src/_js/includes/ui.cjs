fef3acc78899f144c7462a2a917c35f167f00454
matt-tyas <matt.tyas@gmail.com>
2022-02-16 22:16:18 +0000

    removed back to top on tablet and down

--- src/_js/includes/ui.cjs
+++ src/_js/includes/ui.cjs
@@ -2,16 +2,16 @@ const $ = require('jquery');
 
 $(function () {
 
-  $('#js-el-c-scroll-to-top').hide();
+  $('#js-el-c-scroll-to-top').removeClass('el-c-back-to-top--is-shown');
     
   $(window).scroll(function(){
     if($(window).scrollTop() < 3999){
-      $('#js-el-c-scroll-to-top').hide();
+      $('#js-el-c-scroll-to-top').removeClass('el-c-back-to-top--is-shown');
     }
     if ($(window).scrollTop() > 4000) {
-        $('#js-el-c-scroll-to-top').show();
+        $('#js-el-c-scroll-to-top').addClass('el-c-back-to-top--is-shown');
     } else {
-        $('.#js-el-c-scroll-to-top').hide();
+        $('.#js-el-c-scroll-to-top').removeClass('el-c-back-to-top--is-shown');
     }
   });
   $('.toggle-link').click(function (e) {
